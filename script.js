4f1be39bca3409752c083811919d986248b1bb22
Martin Kiprotich <martinkiprotich.mk@gmail.com>
2022-10-10 22:33:05 +0300

    Revert "Completed QR code challenge"

---
+++ script.js
@@ -0,0 +1,16 @@
+let add = document.querySelector("#add");
+let subtract = document.querySelector("#subtract");
+
+add.addEventListener("click", function () {
+  let output = document.querySelector("#output");
+  let result = Number(output.innerText) + 1;
+
+  output.innerText = result;
+});
+
+subtract.addEventListener("click", function () {
+  let output = document.querySelector("#output");
+  let result = Number(output.innerText) - 1;
+
+  output.innerText = result;
+});
