416acc49a41b13320613281c6dbe11e05fbd8c7b
Martin Kiprotich <martinkiprotich.mk@gmail.com>
2022-10-18 12:58:19 +0300

    Fixed bug in functions.js

--- week-2/js-tasks/js-functions/functions.js
+++ week-2/js-tasks/js-functions/functions.js
@@ -6,12 +6,12 @@ link: "https://docs.google.com/document/d/1JlCHJ0D_6qbiFoY_EyFMwC2T3C6Itqfh15Wd4
 // ========== Task 2 ==========
 function countDown(fromNumber) {
   if (fromNumber > 0) {
-    if (fromNumber % 3 === 0) {
+    if (fromNumber % 3 === 0 && fromNumber % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (fromNumber % 3 === 0) {
       console.log("Fizz");
     } else if (fromNumber % 5 === 0) {
       console.log("Buzz");
-    } else if (fromNumber % 3 === 0 && fromNumber % 5 === 0) {
-      console.log("FizzBuzz");
     } else {
       console.log(fromNumber);
     }
